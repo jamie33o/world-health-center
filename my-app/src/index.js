d2fcfcc9d70cbd67c59dab3795962124ee65b8ec
jamie33o <123118240+jamie33o@users.noreply.github.com>
2024-01-05 00:09:30 +0000

    react tutorial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './bootstrap.min.css';
 import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.render(
- <Provider>
+  <Provider store={store}>
     <App />
-    </Provider>,
+  </Provider>,
   document.getElementById('root')
 );
 
